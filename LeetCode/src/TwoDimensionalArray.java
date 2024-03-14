264eba930feba21f3d88bd0d6d46605f6f62c6d9
Alec-Lee777 <140378249+Alec-Lee777@users.noreply.github.com>
2024-03-14 21:19:55 +0800

    二维数组学习及习题

--- LeetCode/src/TwoDimensionalArray.java
+++ LeetCode/src/TwoDimensionalArray.java
@@ -127,10 +127,49 @@ public class TwoDimensionalArray {
             return result;
         }
 
+        // 杨辉三角
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> res = new ArrayList<>();
+            for (int i = 0; i < numRows; i++) {
+                res.add(new ArrayList<>());
+                for (int j = 0; j < i+1; j++) {
+                    if(j == 0 || j == i){
+                        res.get(i).add(1);
+                    }else{
+                        res.get(i).add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public List<Integer> getRow(int rowIndex) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int i = 1; i < rowIndex+1; i++) {
+                row.add(0);
+                for (int j = i; j >= 0; j--) {
+                    if( j == 0 || j == i){
+                        row.set(j,1);
+                    }else{
+                        row.set(j, row.get(j-1)+row.get(j));
+                    }
+                }
+            }
+
+            return row;
+        }
+
     }
 
 
     public static void main(String[] args) {
+
+        int numRows = 7;
+        List<Integer> list = new Solution().getRow(numRows);
+        System.out.println(list);
+
 
     }
 }
